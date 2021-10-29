b7b308140e8f27e698be31b48471ed82b589c65a
shaillyshah7 <68048841+shaillyshah7@users.noreply.github.com>
2021-10-29 15:16:30 +0530

    committed

---
+++ constants/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  defaultServerResponse: {
+    status: 400,
+    message: '',
+    body: {}
+  },
+  productMessage: {
+    PRODUCT_CREATED: 'Product Created Successfully',
+    PRODUCT_FETCHED: 'Product Fetched Successfully',
+    PRODUCT_UPDATED: 'Product Updated Successfully',
+    PRODUCT_DELETED: 'Product Deleted Successfully',
+    PRODUCT_NOT_FOUND: 'Product Not Found'
+  },
+ 
+  requestValidationMessage: {
+    BAD_REQUEST: 'Invalid fields',
+    
+  },
+  databaseMessage: {
+    INVALID_ID: 'Invalid Id'
+  }
+}
---
+++ routes/productRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controller/productController');
+const joiSchemaValidation = require('../middleware/joiSchemaValidation');
+const productSchema = require('../apiSchema/productSchema');
+
+
+router.post('/',
+  joiSchemaValidation.validateBody(productSchema.createProductSchema),
+  productController.createProduct
+);
+
+router.get('/:id',
+  productController.getProductById
+);
+
+router.put('/:id',
+  joiSchemaValidation.validateBody(productSchema.updateProductSchema),
+  productController.updateProduct
+);
+
+router.get('/',
+  joiSchemaValidation.validateQueryParams(productSchema.getAllProductSchema),
+  productController.getAllProducts
+);
+
+router.delete('/:id',
+  productController.deleteProduct
+)
+
+module.exports = router;
